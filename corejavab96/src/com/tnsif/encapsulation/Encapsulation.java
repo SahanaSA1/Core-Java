cb11c446c06d251ddc79c06a153b894b326c3741
Sahana <Sahana@LENOVO-DESKTOP>
2024-06-28 07:11:43 +0530

    second commit

---
+++ corejavab96/src/com/tnsif/encapsulation/Encapsulation.java
@@ -0,0 +1,15 @@
+ //Program to demonstrate encapsulation
+package com.tnsif.encapsulation;
+
+public class Encapsulation {
+	int serialNum=10;//Data variable
+	String name;
+	int age;
+	
+	void show() //Member function
+	{
+		System.out.println("serialnumber"+serialNum+"\t Name"+name+"\t Age:"+ age);
+		
+	}
+	
+}
